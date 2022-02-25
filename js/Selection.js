c45a98ed2a26cf65916da263b31f5cf92143fa4f
rasipriyanka <priyankapendela111@gmail.com>
2022-02-25 21:54:40 +0530

    Implementing the single digit number

---
+++ js/Selection.js
@@ -0,0 +1,39 @@
+//------Read a single digit number and write the number in word using Case------//
+//------Generates Random number between o to 9------------//
+var randomNumber = Math.floor(Math.random() * 10);
+//------Switch Case--------//
+switch (randomNumber) {
+    case 0:
+        console.log("Zero");
+        break;
+    case 1:
+        console.log("One");
+        break;
+    case 2:
+        console.log("Two");
+        break;
+    case 3:
+        console.log("Three");
+        break;
+    case 4:
+        console.log("Four");
+        break;
+    case 5:
+        console.log("Five");
+        break;
+    case 6:
+        console.log("Six");
+        break;
+    case 7:
+        console.log("Seven");
+        break;
+    case 8:
+        console.log("Eight");
+        break;
+    default:
+        console.log("Nine");
+        break;
+
+}
+
+
